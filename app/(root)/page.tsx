85e3e759b4710cd70b51b60165f79bff1976fa3e
Hwanhee Chae <hh@hwan.me>
2025-02-08 13:38:02 +0900

    normalize Symbol component, add sacred symbol calc

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Symbol from "../components/Symbol";
 
 export default function Home() {
@@ -5,7 +7,43 @@ export default function Home() {
         <div className="flex-1">
             {/* here put more components */}
             {/* <Date /> */}
-            <Symbol />
+            <Symbol
+                name="Arcane"
+                shortName="arc"
+                maxLevel={20}
+                maxEquip={2679}
+                dailyDefault={20}
+                weeklyDefault={45}
+                comment="The following calculation assumes Reverse City and Yum Yum Island are open"
+                calculateGrowth={(level) => {
+                    return level ** 2 + 11;
+                }}
+                calculateMesoCost={(level, constant, growth) => {
+                    return (
+                        Math.floor(growth * 2 * (constant + 3 + level * 0.05)) *
+                        10000
+                    );
+                }}
+            />
+            <Symbol
+                name="Sacred"
+                shortName="sac"
+                maxLevel={11}
+                maxEquip={4565}
+                dailyDefault={10}
+                weeklyDefault={0}
+                comment="Regardless of the daily amount, 10 Cernium symbols are included in the calculation."
+                calculateGrowth={(level) => {
+                    return 9 * level ** 2 + 20 * level;
+                }}
+                calculateMesoCost={(level, constant, growth) => {
+                    return (
+                        Math.floor(
+                            growth * 1.8 * (constant + 6 - (level - 1) / 3)
+                        ) * 100000
+                    );
+                }}
+            />
         </div>
     );
 }
--- app/components/Symbol.tsx
+++ app/components/Symbol.tsx
@@ -2,15 +2,39 @@
 
 import React, { useEffect, useState } from "react";
 
-function Symbol() {
-    const [arcDaily, setArcDaily] = useState(true);
-    const [arcPerDay, setArcPerDay] = useState(20);
-    const [arcDailyToday, setArcDailyToday] = useState(true);
-    const [arcWeekly, setArcWeekly] = useState(true);
-    const [arcWeeklyThisWeek, setArcWeeklyThisWeek] = useState(true);
-    // const [arcExtra, setArcExtra] = useState(false);
-    // const [arcExtraAmount, setArcExtraAmount] = useState(50);
-    const [arcInput, setArcInput] = useState([
+function Symbol({
+    name: symbolName,
+    shortName: symbolShortName,
+    maxLevel: symbolMaxLevel,
+    maxEquip: symbolMaxEquip,
+    comment,
+    dailyDefault: symbolDailyDefault,
+    weeklyDefault: symbolWeeklyDefault,
+    calculateGrowth,
+    calculateMesoCost,
+}: {
+    name: string;
+    shortName: string;
+    maxLevel: number;
+    maxEquip: number;
+    dailyDefault: number;
+    weeklyDefault: number;
+    comment: string;
+    calculateGrowth: (level: number) => number;
+    calculateMesoCost: (
+        level: number,
+        constant: number,
+        growth: number
+    ) => number;
+}) {
+    const [symbolDaily, setSymbolDaily] = useState(true);
+    const [symbolPerDay, setSymbolPerDay] = useState(symbolDailyDefault);
+    const [symbolDailyToday, setSymbolDailyToday] = useState(true);
+    const [symbolWeekly, setSymbolWeekly] = useState(symbolWeeklyDefault > 0);
+    const [symbolWeeklyThisWeek, setSymbolWeeklyThisWeek] = useState(true);
+    // const [symbolExtra, setSymbolExtra] = useState(false);
+    // const [symbolExtraAmount, setSymbolExtraAmount] = useState(50);
+    const [symbolInput, setSymbolInput] = useState([
         [-1],
         [1, 0],
         [1, 0],
@@ -19,7 +43,7 @@ function Symbol() {
         [1, 0],
         [1, 0],
     ]);
-    const [arcRemaining, setArcRemaining] = useState([
+    const [symbolRemaining, setSymbolRemaining] = useState([
         [-1],
         [0, 0],
         [0, 0],
@@ -28,157 +52,173 @@ function Symbol() {
         [0, 0],
         [0, 0],
     ]);
-    const [arcMaxCost, setArcMaxCost] = useState([-1, 0]);
+    const [symbolMaxCost, setSymbolMaxCost] = useState([-1, 0]);
 
-    function setArcLevel(i: number, level: number) {
-        const newArcInput = [...arcInput];
-        newArcInput[i][0] = level;
-        setArcInput(newArcInput);
+    function setSymbolLevel(i: number, level: number) {
+        const newSymbolInput = [...symbolInput];
+        newSymbolInput[i][0] = level;
+        setSymbolInput(newSymbolInput);
     }
 
-    function setArcEquip(i: number, equip: number) {
-        const newArcInput = [...arcInput];
-        newArcInput[i][1] = equip;
-        setArcInput(newArcInput);
+    function setSymbolEquip(i: number, equip: number) {
+        const newSymbolInput = [...symbolInput];
+        newSymbolInput[i][1] = equip;
+        setSymbolInput(newSymbolInput);
     }
 
-    function calculateArcRemainingUntilMax() {
-        const newRemaining = [...arcRemaining];
+    function calculateSymbolRemainingUntilMax() {
+        const newRemaining = [...symbolRemaining];
         for (let i = 1; i <= 6; i++) {
             newRemaining[i][0] = 0;
             newRemaining[i][1] = 0;
-            for (let arclevel = arcInput[i][0]; arclevel < 20; arclevel++) {
-                const growth = arclevel ** 2 + 11;
+            for (
+                let symbolLevel = symbolInput[i][0];
+                symbolLevel < symbolMaxLevel;
+                symbolLevel++
+            ) {
+                const growth = calculateGrowth(symbolLevel);
                 newRemaining[i][0] += growth;
-                newRemaining[i][1] +=
-                    Math.floor(growth * 2 * (i + 3 + arclevel * 0.05)) * 10000;
+                newRemaining[i][1] += calculateMesoCost(symbolLevel, i, growth);
             }
-            newRemaining[i][0] -= arcInput[i][1];
+            newRemaining[i][0] -= symbolInput[i][1];
         }
-        setArcRemaining(newRemaining);
+        setSymbolRemaining(newRemaining);
     }
 
-    function calculateDaysToMax(remaining: number) {
-        if (!arcDaily && !arcWeekly) {
+    function calculateDaysToMax(remaining: number, additionalPerDay?: number) {
+        const termination1 = !symbolDaily && !symbolWeekly;
+        const termination2 = symbolDaily && symbolPerDay === 0;
+        if (termination1 || termination2) {
             return -1;
         }
 
-        if (!arcDailyToday) {
-            remaining -= arcPerDay;
+        if (!symbolDailyToday) {
+            remaining -= symbolPerDay + (additionalPerDay || 0);
         }
-        if (!arcWeeklyThisWeek) {
-            remaining -= 45;
+        if (!symbolWeeklyThisWeek) {
+            remaining -= symbolWeeklyDefault;
         }
 
         let days = 0;
 
         while (remaining > 0) {
             days++;
-            if (arcDaily) remaining -= arcPerDay;
-            if (arcWeekly && days % 7 === 0) remaining -= 45;
+            if (symbolDaily)
+                remaining -= symbolPerDay + (additionalPerDay || 0);
+            if (symbolWeekly && days % 7 === 0)
+                remaining -= symbolWeeklyDefault;
         }
 
         return days;
     }
 
     useEffect(() => {
-        calculateArcRemainingUntilMax();
-    }, [arcInput]);
+        calculateSymbolRemainingUntilMax();
+    }, [symbolInput]);
 
     useEffect(() => {
-        const newArcMaxCost = [-1, 0];
+        console.log("test");
+        const newSymbolMaxCost = [-1, 0];
 
         // Find maximum days to max and total cost
         for (let i = 1; i <= 6; i++) {
-            if (arcRemaining[i][0] > newArcMaxCost[0]) {
-                newArcMaxCost[0] = Math.max(
-                    calculateDaysToMax(arcRemaining[i][0]),
-                    newArcMaxCost[0]
+            if (symbolRemaining[i][0] > newSymbolMaxCost[0]) {
+                newSymbolMaxCost[0] = Math.max(
+                    i === 1 && symbolShortName === "sac"
+                        ? calculateDaysToMax(symbolRemaining[i][0], 10)
+                        : calculateDaysToMax(symbolRemaining[i][0]),
+                    newSymbolMaxCost[0]
                 );
             }
-            newArcMaxCost[1] += arcRemaining[i][1];
+            newSymbolMaxCost[1] += symbolRemaining[i][1];
         }
-        setArcMaxCost(newArcMaxCost);
+        setSymbolMaxCost(newSymbolMaxCost);
     }, [
-        arcRemaining,
-        arcDaily,
-        arcPerDay,
-        arcDailyToday,
-        arcWeekly,
-        arcWeeklyThisWeek,
+        symbolRemaining,
+        symbolDaily,
+        symbolPerDay,
+        symbolDailyToday,
+        symbolWeekly,
+        symbolWeeklyThisWeek,
     ]);
 
     return (
         <div className="p-3">
-            <h1 className="text-primary text-xl font-bold mb-2">
-                Arcane Symbol
-            </h1>
+            <div className="flex items-center mb-1">
+                <img
+                    src={`/image/${symbolShortName}0.webp`}
+                    className="w-9 h-9"
+                />
+                <h1 className="text-primary text-xl font-bold ml-1">
+                    {symbolName} Symbol
+                </h1>
+            </div>
             <div className="bg-primaryGray rounded-xl p-3">
                 <div>
                     <input
                         type="checkbox"
-                        id="arcdaily"
                         defaultChecked
                         onChange={() => {
-                            setArcDaily(!arcDaily);
+                            setSymbolDaily(!symbolDaily);
                         }}
                     />
-                    <span className="mx-2">Arcane daily</span>
+                    <span className="mx-2">{symbolName} daily</span>
                     <input
                         type="number"
-                        defaultValue={20}
+                        min={symbolDailyDefault}
+                        defaultValue={symbolDailyDefault}
                         className="text-gray-800 w-11 text-center"
-                        id="arcperday"
                         onChange={(e) => {
-                            setArcPerDay(parseInt(e.target.value));
+                            setSymbolPerDay(parseInt(e.target.value));
                         }}
                     />
                     <span className="mx-2">per day</span>
                     <input
                         type="checkbox"
-                        id="arcdailytoday"
                         defaultChecked
                         onChange={() => {
-                            setArcDailyToday(!arcDailyToday);
+                            setSymbolDailyToday(!symbolDailyToday);
                         }}
                     />
                     <span className="mx-2">completed today</span>
                 </div>
-                <div>
-                    <input
-                        type="checkbox"
-                        id="arcweekly"
-                        defaultChecked
-                        onChange={() => {
-                            setArcWeekly(!arcWeekly);
-                        }}
-                    />
-                    <span className="mx-2">Arcane weekly</span>
-                    <input
-                        type="checkbox"
-                        id="arcweeklythisweek"
-                        defaultChecked
-                        onChange={() => {
-                            setArcWeeklyThisWeek(!arcWeeklyThisWeek);
-                        }}
-                    />
-                    <span className="mx-2">completed this week</span>
-                </div>
+                {symbolWeeklyDefault > 0 ? (
+                    <div>
+                        <input
+                            type="checkbox"
+                            defaultChecked
+                            onChange={() => {
+                                setSymbolWeekly(!symbolWeekly);
+                            }}
+                        />
+                        <span className="mx-2">{symbolName} weekly</span>
+                        <input
+                            type="checkbox"
+                            defaultChecked
+                            onChange={() => {
+                                setSymbolWeeklyThisWeek(!symbolWeeklyThisWeek);
+                            }}
+                        />
+                        <span className="mx-2">completed this week</span>
+                    </div>
+                ) : null}
                 <div className="mt-4">
-                    <input type="checkbox" id="arcextra" disabled />
+                    <input type="checkbox" disabled />
                     <span className="mx-2">Normalize with</span>
                     <input
                         type="number"
                         defaultValue={50}
                         className="text-gray-800 w-11 text-center"
-                        id="arcextraamount"
                         disabled
                     />
                     <span className="mx-2">extra symbol(s)</span>
                 </div>
                 <div className="flex mt-4">
                     <div className="flex flex-col items-center">
-                        <img src="/image/arc1.webp" className="opacity-0" />
+                        <img
+                            src={`/image/${symbolShortName}0.webp`}
+                            className="opacity-0"
+                        />
                         <div>Level</div>
                         <div>Equip</div>
                     </div>
@@ -188,29 +228,27 @@ function Symbol() {
                             key={i}
                         >
                             <img
-                                src={`/image/arc${i}.webp`}
+                                src={`/image/${symbolShortName}${i}.webp`}
                                 className="w-9 h-9"
                             />
                             <input
                                 type="number"
                                 min={1}
-                                max={20}
+                                max={symbolMaxLevel}
                                 defaultValue="1"
                                 className="text-gray-800 w-14 text-center"
-                                id={`arc${i}level`}
                                 onChange={(e) => {
-                                    setArcLevel(i, parseInt(e.target.value));
+                                    setSymbolLevel(i, parseInt(e.target.value));
                                 }}
                             />
                             <input
                                 type="number"
                                 min={0}
-                                max={2679}
+                                max={symbolMaxEquip}
                                 defaultValue={0}
                                 className="text-gray-800 w-14 text-center"
-                                id={`arc${i}equip`}
                                 onChange={(e) => {
-                                    setArcEquip(i, parseInt(e.target.value));
+                                    setSymbolEquip(i, parseInt(e.target.value));
                                 }}
                             />
                         </div>
@@ -218,58 +256,57 @@ function Symbol() {
                 </div>
                 <div className="mt-4">
                     <span className="text-gray-400 italic text-sm">
-                        The following calculation assumes Reverse City and Yum
-                        Yum Island are open
+                        {comment}
                     </span>
-                    {arcRemaining.slice(1).map((arc, i) => (
-                        <div key={i}>
-                            <img
-                                src={`/image/arc${i + 1}.webp`}
-                                className="w-9 h-9 inline-block mr-1"
-                            />
-                            will finish in
-                            <span className="text-primary">
-                                {" "}
-                                {calculateDaysToMax(arc[0])}{" "}
-                            </span>
-                            days and cost
-                            <span className="text-primary">
-                                {" "}
-                                {arc[1].toLocaleString("en-US")}{" "}
-                            </span>
-                            mesos
-                        </div>
-                    ))}
-                    {arcMaxCost[0] === -1 ? (
+                    {symbolMaxCost[0] !== -1
+                        ? symbolRemaining.slice(1).map((cost, i) =>
+                              cost[0] > 0 ? (
+                                  <div key={i}>
+                                      <img
+                                          src={`/image/${symbolShortName}${
+                                              i + 1
+                                          }.webp`}
+                                          className="w-9 h-9 inline-block mr-1"
+                                      />
+                                      will finish in
+                                      <span className="text-primary">
+                                          {" "}
+                                          {i === 0 && symbolShortName === "sac"
+                                              ? calculateDaysToMax(cost[0], 10)
+                                              : calculateDaysToMax(
+                                                    cost[0]
+                                                )}{" "}
+                                      </span>
+                                      days and cost
+                                      <span className="text-primary">
+                                          {" "}
+                                          {cost[1].toLocaleString("en-US")}{" "}
+                                      </span>
+                                      mesos
+                                  </div>
+                              ) : null
+                          )
+                        : null}
+                    {symbolMaxCost[1] === 0 ? (
+                        <div className="text-primary">Upgrade complete!</div>
+                    ) : symbolMaxCost[0] === -1 ? (
                         <div>Cannot upgrade any symbol</div>
                     ) : (
                         <div>
                             The max upgrade will finish in
                             <span className="text-primary">
                                 {" "}
-                                {arcMaxCost[0]}{" "}
+                                {symbolMaxCost[0]}{" "}
                             </span>
                             days and cost
                             <span className="text-primary">
                                 {" "}
-                                {arcMaxCost[1].toLocaleString("en-US")}
+                                {symbolMaxCost[1].toLocaleString("en-US")}
                             </span>{" "}
                             mesos
                         </div>
                     )}
                 </div>
-            </div>
-
-            <h1 className="text-primary text-xl font-bold mt-2 mb-2">
-                Sacred Symbol
-            </h1>
-            <div className="bg-primaryGray rounded-xl p-3">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
             </div>
         </div>
     );
